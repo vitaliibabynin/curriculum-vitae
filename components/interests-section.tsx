a359adbb3bcb126d5ce2af84cacd58792fc8b716
Vitalii <vitalii.babynin@gmail.com>
2024-10-14 01:26:21 +0200

    Next.js Image optimization tweak

--- components/interests-section.tsx
+++ components/interests-section.tsx
@@ -11,7 +11,7 @@ const InterestsSection: React.FC<InterestsSectionProps> = ({ description, imageU
       <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg p-6 flex flex-col md:flex-row items-center">
         <div className="md:w-1/2 mb-6 md:mb-0 md:mr-6">
           <blockquote className="text-gray-700 dark:text-gray-300 italic border-l-4 border-blue-500 pl-4">
-            {"\"" + description + "\""}
+            {`"${description}"`}
           </blockquote>
         </div>
         <div className="md:w-1/2">
@@ -21,6 +21,7 @@ const InterestsSection: React.FC<InterestsSectionProps> = ({ description, imageU
             width={400}
             height={300}
             className="rounded-lg"
+            unoptimized
           />
         </div>
       </div>
